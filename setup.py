b6683dd6c1c6e2214d10bdf548291e42204db1d4
xschen <xs0040@gmail.com>
2018-05-17 13:24:04 +0800

    project setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='pyspark_alg',
+      version='0.0.1',
+      description='Advanced Algorithms in PySpark',
+      url='http://github.com/chen0040/pyspark-advanced-algorithms',
+      author='Xianshun Chen',
+      author_email='xs0040@gmail.com',
+      packages=['pyspark_alg'],
+      zip_safe=False)
